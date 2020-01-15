e72462395560c2030047c8baab451b871fa49f01
Mustafa Refaey <mustafarefaey@gmail.com>
2020-01-15 16:15:00 +0200

    add skuCount state variable

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -12,6 +12,7 @@ contract SupplyChain {
   address owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
+  uint skuCount;
 
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
