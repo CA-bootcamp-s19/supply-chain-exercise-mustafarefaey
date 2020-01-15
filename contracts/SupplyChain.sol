07e067f1655872060bff35e6b34faa0124cb5275
Mustafa Refaey <mustafarefaey@gmail.com>
2020-01-15 16:33:25 +0200

    implement receiveItem function

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -151,8 +151,13 @@ contract SupplyChain {
   /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
   is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
   function receiveItem(uint sku)
-    public
-  {}
+    public shipped(sku) verifyCaller(items[sku].buyer)
+  {
+    // Change the state of the item to received
+    items[sku].state = State.Received;
+
+    emit LogReceived(sku);
+  }
 
   /* We have these functions completed so we can run tests, just ignore it :) */
   function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) {
