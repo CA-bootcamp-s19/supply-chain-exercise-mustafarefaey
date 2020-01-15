41b112cf3e0b0e40edf5126014b297efd97703be
Mustafa Refaey <mustafarefaey@gmail.com>
2020-01-15 16:15:22 +0200

    add items mapping

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -17,6 +17,7 @@ contract SupplyChain {
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
+  mapping(uint => Item) public items;
 
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
